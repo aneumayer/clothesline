5735f7a503e761b0369e01f135aa41080e2024e1
Anthony Neumayer <aneumayer@gmail.com>
2017-02-25 21:39:37 -0500

    removed db session for now

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-    #If there is no action in the url default to home
+    # Include the configuration settings
     require_once("config.php");
+    
+    # Include the database class an create a global object
     require_once("objects/db.php");
-    require_once("objects/session.php");
+    $db = new db();
+
+    # Setup custom session management with database
+    /*require_once("objects/session.php");
     session_set_save_handler(
         array('session', 'open'),
         array('session', 'close'),
@@ -10,9 +15,10 @@
         array('session', 'write'),
         array('session', 'destroy'),
         array('session', 'gc')
-    );
+    );*/
     session_name($config["app"]["sess"]);
     session_start();
-    $db = new db();
+    
+    # Include the controller for handling actions
     require_once("controller.php");
 ?>
--- objects/session.php
+++ objects/session.php
@@ -2,41 +2,47 @@
 
 class session {
     
-    public function open($save_path, $session_name) {
+    public static function open($save_path, $session_name) {
         return true;
     }
     
-    public function close() {
+    public static function close() {
         # No code needed
     }
     
-    public function read($session_id) {
+    public static function read($session_id) {
         global $db;
         $session_id = $db->escape_string($session_id);
         $result = $db->query("SELECT * FROM session WHERE session_id = '{$session_id}'");
-        if(isarray($result)) {
-            return $result["session_data"];
+        if(is_array($result)) {
+            if(isset($result[0]["session_data"])) {
+                $return_value = $result[0]["session_data"];
+            } else {
+                $return_value = "";
+            }
         } else {
-            return "";
+            $return_value = "";
         }
+        return $return_value;
     }
     
-    public function write($session_id, $session_data) {
+    public static function write($session_id, $session_data) {
         global $db;
         $session_id = $db->escape_string($session_id);
         $session_data = $db->escape_string($session_data);
-        $db->query("DELETE FROM session WHERE session_id = '{$sesion_id}'");
-        $db->query("INSERT INTO session (session_id, last_update, session_data) VALUES ('{$session_id}', NOW(), '{$sesssion_data}')");
+        $db->query("DELETE FROM session WHERE session_id = '{$session_id}'");
+        $db->query("INSERT INTO session (session_id, last_update, session_data) VALUES ('{$session_id}', NOW(), '{$session_data}')");
         return true;
     }
     
-    public function destroy($session_id) {
+    public static function destroy($session_id) {
+        global $db;
         $session_id = $db->escape_string($session_id);
-        $db->query("DELETE FROM session WHERE session_id = '{$sesion_id}'");
+        $db->query("DELETE FROM session WHERE session_id = '{$session_id}'");
         return true;
     }
     
-    public function gc($max_lifetime) {
+    public static function gc($max_lifetime) {
         global $db;
         $max_date = date('Y-m-d H:m:s', time() - $max_lifetime);
         $db->query("DELETE FROM session WHERE last_update < '{$max_date}'");

68b40f8b67c80558af67f1ce2e671052b6f8fb5c
Anthony Neumayer <aneumayer@gmail.com>
2017-11-15 12:00:14 -0500

    Removed accidental code

--- db-models/User.php
+++ db-models/User.php
@@ -3,10 +3,6 @@
 class User extends ActiveRecord\Model
 {
     static $table_name  = 'user';
-    static $has_many = array(
-        array('category', 'class_name' => 'Property'),
-        array('subscriptions', 'class_name' => 'Subscription', ),
-    );
 }
 
 ?>

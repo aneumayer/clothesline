5441761d73cc6b5ab8fb51ba38218e2da7c230fc
Anthony Neumayer <aneumayer@gmail.com>
2017-11-20 15:35:02 -0500

    Updated header

--- views/vw_header.php
+++ views/vw_header.php
@@ -1,36 +1,35 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo $config["app"]["title"] . (strlen($page_title) ? " - {$page_title}" : ""); ?></title>
+        <title><?= $config["app"]["title"] . (strlen($page_title) ? " - {$page_title}" : ""); ?></title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/main.css" rel="stylesheet">
         <script type="text/javascript" src="js/jquery-3.1.1.min.js" ></script>
         <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <table id="header">
-            <tr>
-                <td id="l_side">
-                    <?php
-                        if(strlen($page_title) && !$_GET["action"] != "login") {
-                            echo("<a href=\"./\">Back</a>");
-                        }
-                    ?>
-                </td>
-                <td id="title">
-                        <?php
-                            echo strlen($page_title) ? $page_title : $config["app"]["title"];
-                        ?>
-                </td>
-                <td id="r_side">
-                    <?php
-                        if($_GET["action"] != "login") {
-                            echo("<a href=\"./?action=logout\">Log Out</a>");
-                        }
-                    ?>
-                </td>
-            </tr>
-        </table>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="./"><?= $config["app"]["title"] ?></a>
+                </div>
+                <?php if ($_GET["action"] != "login") : ?>
+                    <ul class="nav navbar-nav">
+                        <li <?= ($_GET['action'] == 'register') ? "class=\"active\"" : "";?>><a href="./?action=register">Register New Load</a></li>
+                        <li <?= ($_GET['action'] == 'check_in') ? "class=\"active\"" : "";?>><a href="./?action=check_in">Check in Load</a></li>
+                        <?php if(isset($_SESSION['admin']) && $_SESSION["admin"] == 1) { ?>
+                            <li <?= ($_GET['action'] == 'track') ? "class=\"active\"" : "";?>><a href="./?action=track">Track Loads</a></li>
+                            <li <?= ($_GET['action'] == 'details') ? "class=\"active\"" : "";?>><a href="./?action=details">Load Details</a></li>
+                            <li <?= ($_GET['action'] == 'rank') ? "class=\"active\"" : "";?>><a href="./?action=rank">Rank Accounts</a></li>
+                        <?php } ?>
+                        <li <?= ($_GET['action'] == 'account') ? "class=\"active\"" : "";?>><a href="./?action=account">My Account</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="./?action=logout"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </nav>
         <?php
             if(isset($success_message)) {
                 echo("<div class=\"success_message\">{$success_message}</div>");

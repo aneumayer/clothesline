9defab0dcb5dd445b5a23515ad14044eb8c3c63c
Anthony Neumayer <aneumayer@gmail.com>
2017-11-20 15:56:39 -0500

    Header updates

--- views/vw_header.php
+++ views/vw_header.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <title><?= $config["app"]["title"] . (strlen($page_title) ? " - {$page_title}" : ""); ?></title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
         <script type="text/javascript" src="js/jquery-3.1.1.min.js" ></script>
-        <script src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-inverse">
@@ -15,16 +15,28 @@
                 </div>
                 <?php if ($_GET["action"] != "login") : ?>
                     <ul class="nav navbar-nav">
-                        <li <?= ($_GET['action'] == 'register') ? "class=\"active\"" : "";?>><a href="./?action=register">Register New Load</a></li>
-                        <li <?= ($_GET['action'] == 'check_in') ? "class=\"active\"" : "";?>><a href="./?action=check_in">Check in Load</a></li>
+                        <li <?= ($_GET['action'] == 'register') ? "class=\"active\"" : "";?>>
+                            <a href="./?action=register">Register New Load</a>
+                        </li>
+                        <li <?= ($_GET['action'] == 'check_in') ? "class=\"active\"" : "";?>>
+                            <a href="./?action=check_in">Check in Load</a>
+                        </li>
                         <?php if(isset($_SESSION['admin']) && $_SESSION["admin"] == 1) { ?>
-                            <li <?= ($_GET['action'] == 'track') ? "class=\"active\"" : "";?>><a href="./?action=track">Track Loads</a></li>
-                            <li <?= ($_GET['action'] == 'details') ? "class=\"active\"" : "";?>><a href="./?action=details">Load Details</a></li>
-                            <li <?= ($_GET['action'] == 'rank') ? "class=\"active\"" : "";?>><a href="./?action=rank">Rank Accounts</a></li>
+                            <li <?= ($_GET['action'] == 'track') ? "class=\"active\"" : "";?>>
+                                <a href="./?action=track">Track Loads</a>
+                            </li>
+                            <li <?= ($_GET['action'] == 'details') ? "class=\"active\"" : "";?>>
+                                <a href="./?action=details">Load Details</a>
+                            </li>
+                            <li <?= ($_GET['action'] == 'rank') ? "class=\"active\"" : "";?>>
+                                <a href="./?action=rank">Rank Accounts</a>
+                            </li>
                         <?php } ?>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="./?action=account"><span class="glyphicon glyphicon-user"></span> Account</a></li>
+                        <li <?= ($_GET['action'] == 'account') ? "class=\"active\"" : "";?>>
+                            <a href="./?action=account"><span class="glyphicon glyphicon-user"></span> Account</a>
+                        </li>
                         <li><a href="./?action=logout"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
                     </ul>
                 <?php endif; ?>

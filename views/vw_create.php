b01855270df89f62043399f921df580447c3bfe2
Anthony Neumayer <aneumayer@gmail.com>
2017-11-22 12:45:48 -0500

    Don't show categories in create if you are not in any

--- views/vw_create.php
+++ views/vw_create.php
@@ -1,6 +1,7 @@
 <header class="text-center">
     <h1><?= (strlen($page_title)) ? $page_title : "Home" ?></h1>
 </header>
+<?php if (count($categories)) : ?>
 <form method="post" action="<?php echo($_SERVER['REQUEST_URI']); ?>">
     <div class="col-md-6 mx-auto">
         <div class="form-group row">
@@ -20,5 +21,10 @@
                 <input type="submit" name="create" value="Create" />
             </div>
         </div>
-    </div.
+    </div>
 </form>
+<?php else : ?>
+    <div class="col-md-12 text-center">
+        <p>You currently have no groups that you are placed in.</p>
+    </div>
+<?php endif; ?>

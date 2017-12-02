26b928d394b6f52f8fff5989cdb47fd6ae42783d
Anthony Neumayer <aneumayer@gmail.com>
2017-12-02 17:05:50 -0500

    Added message to main page

--- views/vw_home.php
+++ views/vw_home.php
@@ -6,6 +6,11 @@
         <p class="h2 mt-5">Welcome!</p>
         <p>To find out where to take a bag of clothes, select "Delivery Address" from the menu above.</p>
         <p>Thank you for sharing clothes with your neighbors!</p>
+        <p> 
+            Help us spread the word about the clothesline by adding  
+            <a href="/wp-content/uploads/2017/10/Clothesline-Flyer.pdf" target="_blank">this flyer</a> 
+            to a community board near you.
+        </p>
         <p>
             Full information about <?= $config['app']['title'] ?> 
             can be found on <a href="https://www.connectionsinc.org/clothesline" target="_blank">our website</a>.
